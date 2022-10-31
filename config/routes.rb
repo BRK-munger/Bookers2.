86f8aaec9a6a160e5eaf54f9fc6cc0bddbea7142
BRK-munger <cassandra.kk.munger@icloud.com>
2022-10-31 09:12:16 +0000

    [Add] フォロー/フォロワー機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root :to =>"homes#top"
@@ -7,7 +9,14 @@ Rails.application.routes.draw do
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
    resources :book_comments, only: [:create,:destroy]
    resource :favorites, only: [:create,:destroy]
-end
+  end
+  
+  #フォロー・フォロワー
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers',as: 'followers'
+  end
+  
   resources :users, only: [:index,:show,:edit,:update]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
