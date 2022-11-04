9042ad37d22db13201f623c0d3a4d2be6be981df
BRK-munger <cassandra.kk.munger@icloud.com>
2022-11-04 13:51:13 +0000

    [Add] レビューサービスの機能実装2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,16 +9,8 @@ class BooksController < ApplicationController
   end
 
   def index
-    if params[:latest]
-      @books = Book.latest
-    elsif params[:old]
-      @books = Book.old
-    elsif params[:star_count]
-      @books = Book.star_count
-    else
-      @books = Book.all
-    end
-      @book = Book.new
+    @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -51,7 +43,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :star)
+    params.require(:book).permit(:title, :body, :rate)
   end
 
   def ensure_correct_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_03_080852) do
+ActiveRecord::Schema.define(version: 2022_11_03_134216) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2022_11_03_080852) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "Book_id"
-    t.string "star"
+    t.float "rate"
   end
 
   create_table "favorites", force: :cascade do |t|
